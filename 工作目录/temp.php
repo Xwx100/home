9e7ca55d0dd03669f7f0283b15cc4f73a3879da9
xuweixin <xuweixin@ibingniao.com>
2020-02-17 20:31:38 +0800

    xu: need_manage add access

--- 工作目录/temp.php
+++ 工作目录/temp.php
@@ -1,15 +1,19 @@
 <?php
 
-function a($a, $b, ...$c) {
-    var_dump($a);
-    var_dump($b);
-    var_dump($c);
-    var_dump(func_get_args());
-    var_dump(func_num_args());
-    var_dump(func_get_arg(6));
-}
+//function a($a, $b, ...$c) {
+//    var_dump($a);
+//    var_dump($b);
+//    var_dump($c);
+//    var_dump(func_get_args());
+//    var_dump(func_num_args());
+//    var_dump(func_get_arg(6));
+//}
+//
+//$b = ['6' => 1,2,3];
+//a(1, ...$b, ...$b);
+//
+//var_dump(is_float(111.0));
 
-$b = ['6' => 1,2,3];
-a(1, ...$b, ...$b);
 
-var_dump(is_float(111.0));
+var_dump([1,2,3] + [2,3,4]);
+var_dump(array_merge([1,2,3], [2,3,4]));
